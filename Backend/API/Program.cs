1a783a5c333eb68d8a3b3cd9058a6c380f8c3982
Yurii Kyrpotenko <50027889+WeiZedInc@users.noreply.github.com>
2024-10-04 18:29:07 +0300

    added cors policy

--- Backend/API/Program.cs
+++ Backend/API/Program.cs
@@ -5,7 +5,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontEndRequests",
+        policy => policy.WithOrigins("https://localhost:7205")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod());
+});
+
 var app = builder.Build();
+
+app.UseCors("AllowFrontEndRequests");
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
